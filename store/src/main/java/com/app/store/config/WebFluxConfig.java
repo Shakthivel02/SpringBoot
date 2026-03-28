3553b8c6dcbafa849240f4f7ddd683a2e3523d78
shakthiUMR <shakthivel@futureeducation.ai>
2026-03-28 18:22:06 +0530

    feat: configure global CORS settings for WebFlux application

---
+++ store/src/main/java/com/app/store/config/WebFluxConfig.java
@@ -0,0 +1,17 @@
+package com.app.store.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
